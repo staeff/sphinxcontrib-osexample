8db4239289526206dc086992cb3d1f2a3acbd4bb
Stephan Klinger <stephan.klinger@freitag.de>
2016-04-02 19:07:31 +0200

    Remove RegexLexer and derive all os examples from pygments BashLexer

--- sphinxcontrib/osexample.py
+++ sphinxcontrib/osexample.py
@@ -4,91 +4,13 @@ Examplecode specs
 =================
 """
 import os
-import re
 from docutils.parsers.rst import Directive
 from docutils import nodes
 from sphinx.util.osutil import copyfile
-from pygments.lexers import get_lexer_by_name
-from pygments.lexer import RegexLexer, bygroups
-from pygments.token import Keyword, Number, Text, Comment
-from pygments.util import ClassNotFound
-from sphinx.highlighting import lexers
+
 
 CSS_FILE = 'osexample.css'
 JS_FILE = 'osexample.js'
-
-__all__ = ['UbuntuLexer', 'CentosLexer', 'FedoraLexer', 'OSXLexer']
-
-class UbuntuLexer(RegexLexer):
-    name = 'Ubuntu'
-    aliases = ['ubuntu', 'debian', 'Debian']
-    filenames = ['*.ubuntu']
-
-    tokens = {
-        'root': [
-        (r'\bapt\b', Keyword),
-        (r'\b((apt-)?get\b)', Keyword),
-        (r'\s', Text),
-        (r'-', Text),
-        (r'\S+', Keyword.Pseudo),
-        (r'[;#].*', Comment.Single),
-        ],
-    }
-
-class CentosLexer(RegexLexer):
-    name = 'Centos'
-    aliases = ['centos', 'CentOS']
-
-    tokens = {
-        'root': [
-        (r'\byum\b', Keyword),
-        (r'\b((yum)?update\b)', Keyword),
-        (r'\b((yum)?install\b)', Keyword),
-        (r'\b((yum-)?check\b)', Keyword),
-        (r'\s', Text),
-        (r'-', Text),
-        (r'\S+', Keyword.Pseudo),
-        (r'[;#].*', Comment.Single),
-        ],
-    }
-
-class FedoraLexer(RegexLexer):
-    name = 'Fedora'
-    aliases = ['fedora']
-
-    tokens = {
-        'root': [
-        (r'\bdnf\b', Keyword),
-        (r'\b((dnf)?update\b)', Keyword),
-        (r'\b((dnf)?install\b)', Keyword),
-        (r'\b((dnf-)?check\b)', Keyword),
-        (r'\s', Text),
-        (r'-', Text),
-        (r'\S+', Keyword.Pseudo),
-        (r'[;#].*', Comment.Single),
-        ],
-    }
-
-class OSXLexer(RegexLexer):
-    name = 'OSX'
-    aliases = ['osx']
-
-    tokens = {
-        'root': [
-        (r'\bbrew\b', Keyword),
-        (r'\b((brew)?install\b)', Keyword),
-        (r'\b((brew)?create\b)', Keyword),
-        (r'\b((brew)?edit\b)', Keyword),
-        (r'\b((brew)?update\b)', Keyword),
-        (r'\b((brew)?upgrade\b)', Keyword),
-        (r'\b((brew)?cleanup\b)', Keyword),
-        (r'\b((installer-)?pkg\b)', Keyword),
-        (r'\s', Text),
-        (r'-', Text),
-        (r'\S+', Keyword.Pseudo),
-        (r'[;#].*', Comment.Single),
-        ],
-    }
 
 class ExampleCodeDirective(Directive):
     """
@@ -111,7 +33,6 @@ class ExampleCodeDirective(Directive):
         self.state.nested_parse(self.content, self.content_offset, node)
         return [node]
 
-
 def add_assets(app):
     app.add_stylesheet(CSS_FILE)
     app.add_javascript(JS_FILE)
@@ -129,16 +50,16 @@ def copy_assets(app, exception):
     app.info('done')
 
 def setup(app):
-    app.add_directive('example-code',  ExampleCodeDirective)
-    app.connect('builder-inited', add_assets)
-    app.connect('build-finished', copy_assets)
     try:
-	get_lexer_by_name('ubuntu')
-	get_lexer_by_name('fedora')
-	get_lexer_by_name('centos')
-    except ClassNotFound:
-	app.add_lexer('Ubuntu', UbuntuLexer())
-	app.add_lexer('Debian', UbuntuLexer())
-	app.add_lexer('Fedora', FedoraLexer())
-	app.add_lexer('CentOS', CentosLexer())
-    app.add_lexer('OSX', OSXLexer())
+        from pygments.lexers import BashLexer
+    except ImportError:
+        pass  # not fatal if we have old (or no) Pygments
+    else:
+        # Add all lexers that should be supported here
+        app.add_lexer('Ubuntu', BashLexer())
+        app.add_lexer('Debian', BashLexer())
+        app.add_lexer('Fedora', BashLexer())
+        app.add_lexer('CentOS', BashLexer())
+        app.add_directive('osexample',  ExampleCodeDirective)
+        app.connect('builder-inited', add_assets)
+        app.connect('build-finished', copy_assets)
